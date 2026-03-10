d459e8519f10dae52bcca292bf3b75b68f07c5eb
[Thana-Toast] <erebe57@gmail.com>
2026-03-10 14:55:29 +0100

    new: not-found route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { Home } from '../pages/home/home.component';
+import { NotFound } from '../pages/not-found/not-found';
 
 // TODO (Seance 4 - routing): declarer ici les routes lazy-loaded vers
 // Accueil, DetailFilm, Favoris et NotFound.
@@ -9,5 +10,11 @@ export const routes: Routes = [
         path: '',
         component: Home,
         title: 'WishFlix - Accueil'
+    },
+    {
+        // ** → pour toute autre route
+        path: '**',
+        component: NotFound,
+        title: "404 - Tu t'es perdu :("
     }
 ];
